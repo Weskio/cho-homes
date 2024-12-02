beb88eb5f8a602ed4a122659903124df54fbb14a
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2024-12-02 14:57:22 +0000

    chore: site layout

---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,18 @@
+import Header from '../header/header'
+import Footer from '../footer/footer'
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
